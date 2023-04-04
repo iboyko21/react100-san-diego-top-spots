c34c9bbb45697b64bad0b87731f32fdfa3ca0772
Igor Boyko <iboyko@gmail.com>
2023-04-04 10:02:23 -0700

    fixed comments

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -5,7 +5,7 @@ export const TopSpot = (props) =>
                     <div className='well card-body'>
                         <h4 className='card-title'>{props.name}</h4>  
                         <p className='card-text text-light p-3'>{props.description}</p>
-                        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`} className='btn btn-warning shadow' target="_blank">Open Google Maps</a>
+                        <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`} 
+                        className='btn btn-warning shadow' target="_blank">Open Google Maps</a>
                     </div>
                 </div>
-            
